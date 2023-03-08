795e4fe27795a407b7fed5ef4272f20c74dcc930
Micaela Perez <micaelaperez663@gmail.com>
2023-03-08 12:51:13 -0300

    compound-components-rick&morty-exercise

--- semana-1/compound-components/src/App.tsx
+++ semana-1/compound-components/src/App.tsx
@@ -1,18 +1,25 @@
 import "./App.css";
-import Card from './components/Card';
-import {characters} from "./data";
+import Card from "./components/Card";
+import { characters } from "./data";
 
 function App() {
-	return (
-		<>
-			<h1>Compound Components</h1>
-			<main>
-				{characters.map((character) => (
-					<Card key={character.id} character={character} />
-				))}
-			</main>
-		</>
-	);
+  return (
+    <>
+      <h1>Compound Components</h1>
+      <main>
+        {characters.map((character) => (
+          <Card key={character.id}>
+			<Card.Body>
+				<Card.Img src={character.image} alt={character.title} />
+				<Card.Title>{character.title}</Card.Title>
+				<Card.Desc>{character.desc}</Card.Desc>
+				<Card.Status>{character.status}</Card.Status>
+			</Card.Body>
+		  </Card>
+        ))}
+      </main>
+    </>
+  );
 }
 
 export default App;
