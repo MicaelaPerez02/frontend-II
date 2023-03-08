b38d776d4b911adb4e10c4bdc71869b0809ea3bf
Micaela Perez <micaelaperez663@gmail.com>
2023-03-08 12:35:25 -0300

    render-props-temperature-exercise

--- semana-1/render-props/src/App.tsx
+++ semana-1/render-props/src/App.tsx
@@ -1,16 +1,20 @@
-import './App.css'
-import { Fahrenheit, Input, Kelvin } from './components'
+import "./App.css";
+import { Fahrenheit, Input, Kelvin } from "./components";
 
 function App() {
-
   return (
     <div className="App">
       <h1>Convertidor de Temperatura⛅</h1>
-      <Input />
-      <Kelvin value={0} />
-      <Fahrenheit value={0} />
+      <Input
+        render={(value) => (
+          <>
+            <Kelvin value={value} />
+            <Fahrenheit value={value} />
+          </>
+        )}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- semana-1/render-props/src/components/Input/Input.tsx
+++ semana-1/render-props/src/components/Input/Input.tsx
@@ -1,17 +1,22 @@
-import React, {useState} from "react";
-export interface InputInterface {}
+import React, { ReactNode, useState } from "react";
+export interface InputInterface {
+  render: (value: number) => ReactNode;
+}
 
-const Input: React.FC<InputInterface> = () => {
-	const [value, setValue] = useState<string>("");
+const Input: React.FC<InputInterface> = ({ render }) => {
+  const [value, setValue] = useState<number>(0);
 
-	return (
-		<input
-			type="text"
-			value={value}
-			onChange={(e) => setValue(e.target.value)}
-			placeholder="Temperatura en C°"
-		/>
-	);
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(Number(e.target.value))}
+        placeholder="Temperatura en C°"
+      />
+	  {render(value)}
+    </>
+  );
 };
 
 export default Input;
